71ab015cd23321762daa0834bdfecf7a8106424a
shjeong4066-debug <shjeong4066@gmail.com>
2026-04-29 10:33:03 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  'https://rdpkmwtwmfjprylzxboe.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkcGttd3R3bWZqcHJ5bHp4Ym9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTk0MDksImV4cCI6MjA5MjkzNTQwOX0.0EXeB-X-0SHRMZWY1ahG8lmjV1Fv1FsjU7T4OqNyfug'
+)
